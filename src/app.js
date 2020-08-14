3226b70d03b74f0a3bea799147ffcf451cfba494
Shelby Gluck <shelbygluck@gmail.com>
2020-08-14 13:48:17 -0400

    specific forecast data sorted in arrays

--- src/app.js
+++ src/app.js
@@ -1,19 +1,44 @@
-import { convertDate } from "./utils";
+import { convertDate } from "./utils"
+import axios from 'axios'
 
-let lon, lat
+let lon, lat, date
+let icon = []
+let summary = []
+let low = []
+let high = []
 
 if (navigator.geolocation) {
     // geolocation is available
     console.log('GL available')
-    navigator.geolocation.getCurrentPosition((position) => {
+    
+    navigator.geolocation.getCurrentPosition(async (position) => {
         lon = position.coords.longitude
         lat = position.coords.latitude
         console.log(lat, lon)
-        // const locationData = axios.get('https://se-weather-api.herokuapp.com/api/v1/geo'position.coords.latitude, position.coords.longitude);
-      });
+        const date = new Date()
+        try {
+            const res = await axios.get(`https://se-weather-api.herokuapp.com/api/v1/forecast?latitude=${lat}&longitude=${lon}&date=${date}`)
+            const forecastData = res.data.daily.data
+            grabWeatherData(forecastData, 3)
+
+        } catch(err) {
+            console.log(err)
+        }
+    });
   } 
   else {
     // geolocation is not supported
     console.log('geolocation not available')
   }
 
+
+
+  function grabWeatherData(weatherData, numOfDays) {
+      for (let i = 0; i < numOfDays; i++) {
+            icon.push(weatherData[i].icon)
+            summary.push(weatherData[i].summary)
+            high.push(weatherData[i].temperatureHigh)
+            low.push(weatherData[i].temperatureLow)
+      }
+      console.log(icon, summary, high, low)
+  }

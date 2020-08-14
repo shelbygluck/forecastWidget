72b22931028332ba985404b19081c8bcc6015638
Shelby Gluck <shelbygluck@gmail.com>
2020-08-14 13:33:32 -0400

    capturing current lat and lon

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 import { convertDate } from "./utils";
 
-console.log("hello world");
+let lon, lat
+
+if (navigator.geolocation) {
+    // geolocation is available
+    console.log('GL available')
+    navigator.geolocation.getCurrentPosition((position) => {
+        lon = position.coords.longitude
+        lat = position.coords.latitude
+        console.log(lat, lon)
+        // const locationData = axios.get('https://se-weather-api.herokuapp.com/api/v1/geo'position.coords.latitude, position.coords.longitude);
+      });
+  } 
+  else {
+    // geolocation is not supported
+    console.log('geolocation not available')
+  }
+

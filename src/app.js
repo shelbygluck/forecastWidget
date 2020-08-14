8e14b1a31f4aad0e772f469893dc58f056ae08d1
Shelby Gluck <shelbygluck@gmail.com>
2020-08-14 14:24:18 -0400

    basic HTML

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import { convertDate } from "./utils"
+import { grabWeatherData } from "./utils"
 import axios from 'axios'
 
-let lon, lat, date
+let lon, lat, city
 let icon = []
 let summary = []
 let low = []
 let high = []
+const today = new Date()
+const dd = today.getDate()
+const mm = today.getMonth()+1
+const yyyy = today.getFullYear()
+const structDate = mm + '/' + dd + '/' + yyyy
+
 
 if (navigator.geolocation) {
     // geolocation is available
@@ -14,31 +20,23 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(async (position) => {
         lon = position.coords.longitude
         lat = position.coords.latitude
-        console.log(lat, lon)
-        const date = new Date()
-        try {
-            const res = await axios.get(`https://se-weather-api.herokuapp.com/api/v1/forecast?latitude=${lat}&longitude=${lon}&date=${date}`)
-            const forecastData = res.data.daily.data
-            grabWeatherData(forecastData, 3)
 
+        try {
+            //consume forecast api with lat and lon, fill forecast category arrays to be iterated over
+            const forecastRes = await axios.get(`https://se-weather-api.herokuapp.com/api/v1/forecast?latitude=${lat}&longitude=${lon}&date=${structDate}`)
+            const forecastData = forecastRes.data.daily.data
+            grabWeatherData(forecastData, 3, icon, summary, high, low)
+
+           //consume reverse geocode api to get city from lat and lon, to be used in title
+           const cityRes = await axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`)
+           city = cityRes.data.city.toUpperCase()
+           document.getElementById('title').innerHTML = `WEATHER FORECAST FOR ${city}`
         } catch(err) {
             console.log(err)
         }
-    });
+    })
   } 
   else {
     // geolocation is not supported
     console.log('geolocation not available')
-  }
-
-
-
-  function grabWeatherData(weatherData, numOfDays) {
-      for (let i = 0; i < numOfDays; i++) {
-            icon.push(weatherData[i].icon)
-            summary.push(weatherData[i].summary)
-            high.push(weatherData[i].temperatureHigh)
-            low.push(weatherData[i].temperatureLow)
-      }
-      console.log(icon, summary, high, low)
   }
--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,13 @@
 export function convertDate(time) {
   return time * 1000;
 }
+
+export function grabWeatherData(weatherData, numOfDays, icon, summary, high, low) {
+    for (let i = 0; i < numOfDays; i++) {
+          icon.push(weatherData[i].icon)
+          summary.push(weatherData[i].summary)
+          high.push(weatherData[i].temperatureHigh)
+          low.push(weatherData[i].temperatureLow)
+    }
+    console.log(icon, summary, high, low)
+}
